71406621112a726b4d344b2f8b0489cebabcaeb5
Денис Котляров <denis2005991@gmail.com>
2018-09-02 16:18:32 +0300

    1. Fix $crate

--- src/macros.rs
+++ src/macros.rs
@@ -2,7 +2,7 @@
 #[macro_export]
 macro_rules! warn {
 	($($arg:tt)*) => (
-		::cluLog::as_log().warning(format_args!( $($arg)* ));		
+		$crate::as_log().warning(format_args!( $($arg)* ));		
 	)
 }
 
@@ -10,7 +10,7 @@ macro_rules! warn {
 #[macro_export]
 macro_rules! inf {
 	($($arg:tt)*) => (
-		::cluLog::as_log().info(format_args!( $($arg)* ));		
+		$crate::as_log().info(format_args!( $($arg)* ));		
 	)
 }
 
@@ -18,14 +18,14 @@ macro_rules! inf {
 #[macro_export]
 macro_rules! err {
 	($($arg:tt)*) => (
-		::cluLog::as_log().error(format_args!( $($arg)* ));	
+		$crate::as_log().error(format_args!( $($arg)* ));	
 	)
 }
 
 #[macro_export]
 macro_rules! trace {
 	($($arg:tt)*) => (
-		::cluLog::as_log().trace(line!(), column!(), file!(), format_args!( $($arg)* ));	
+		$crate::as_log().trace(line!(), column!(), file!(), format_args!( $($arg)* ));	
 	)
 }
 
@@ -33,7 +33,7 @@ macro_rules! trace {
 #[macro_export]
 macro_rules! panic {
 	($($arg:tt)*) => (
-		::cluLog::as_log().panic(format_args!( $($arg)* ));
+		$crate::as_log().panic(format_args!( $($arg)* ));
 	)
 }
 
@@ -73,10 +73,10 @@ macro_rules! log {
 #[macro_export]
 macro_rules! unk {
 	(?, $($arg:tt)*) => (
-		::cluLog::as_log().unknown("UNK", format_args!( $($arg)* ));
+		$crate::as_log().unknown("UNK", format_args!( $($arg)* ));
 	);
 	($name:expr, $($arg:tt)*) => (
-		::cluLog::as_log().unknown($name, format_args!( $($arg)* ));
+		$crate::as_log().unknown($name, format_args!( $($arg)* ));
 	);
 }
 
@@ -104,10 +104,10 @@ macro_rules! lock {
 #[macro_export]
 macro_rules! lock_out {
 	() => {
-		::cluLog::as_log().lock_out()
+		$crate::as_log().lock_out()
 	};
 	(no_flush) => {
-		::cluLog::as_log().no_flush_lock_out()
+		$crate::as_log().no_flush_lock_out()
 	};
 }
 
@@ -115,10 +115,10 @@ macro_rules! lock_out {
 #[macro_export]
 macro_rules! lock_err {
 	() => {
-		::cluLog::as_log().lock_err()
+		$crate::as_log().lock_err()
 	};
 	(no_flush) => {
-		::cluLog::as_log().no_flush_lock_err()
+		$crate::as_log().no_flush_lock_err()
 	};
 }
 
@@ -126,14 +126,14 @@ macro_rules! lock_err {
 #[macro_export]
 macro_rules! flush {
 	() => (
-		::cluLog::as_log().flush();
+		$crate::as_log().flush();
 	);
 	
 	(out) => (
-		::cluLog::as_log().flush_out();
+		$crate::as_log().flush_out();
 	);
 	(err) => (
-		::cluLog::as_log().flush_err();
+		$crate::as_log().flush_err();
 	);
 }
 #[macro_export]
@@ -154,7 +154,7 @@ macro_rules! flush_err {
 #[macro_export]
 macro_rules! print {
 	($($arg:tt)*) => {
-		::cluLog::as_log().print( format_args!($($arg)*) );
+		$crate::as_log().print( format_args!($($arg)*) );
 	}
 }
 
@@ -175,7 +175,7 @@ macro_rules! println {
 #[macro_export]
 macro_rules! eprint {
 	($($arg:tt)*) => {
-		::cluLog::as_log().eprint( format_args!( $($arg)* ) );
+		$crate::as_log().eprint( format_args!( $($arg)* ) );
 	}
 }
 
